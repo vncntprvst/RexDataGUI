db70f38ccf14432f8c4ccd28f1319e136888f9a6
Vincent Prevosto <vincent@server.com>
2013-05-02 01:21:47 -0400

    comparative figure of IF / tach
    figure with Tachometric curve, inhibition function (with representative
    SSDs only), distribution of rPTs and RTs

--- cmd_wilco_cancellation.m
+++ cmd_wilco_cancellation.m
@@ -1,4 +1,4 @@
-function [p_cancellation,h_cancellation] = cmd_wilco_cancellation(recname,datalign)
+function [p_cancellation,h_cancellation,xctr] = cmd_wilco_cancellation(recname,datalign)
 
 
     %% first get SSDs and SSRT, to later parse latency-matched trials and CSS according to SSDs
@@ -48,20 +48,67 @@ function [p_cancellation,h_cancellation] = cmd_wilco_cancellation(recname,datali
     
     %% get tychometric curve
     SSDRTs=inf(length([datalign(1,1).trials datalign(1,2).trials datalign(1,3).trials]),3); % may differ from sum(~allbad)
-    SSDRTs(datalign(1,2).trials,1)=datalign(2).ssd(:,1);
-    SSDRTs(datalign(1,3).trials,1)=datalign(3).ssd(:,1);
+    ccssd(find(ismember(ccssd,unique(ccssd-1))))=ccssd(find(ismember(ccssd,unique(ccssd-1))))+1;
+    SSDRTs(datalign(1,2).trials,1)=ccssd;
+    nccssd(find(ismember(nccssd,unique(nccssd-1))))=nccssd(find(ismember(nccssd,unique(nccssd-1))))+1;
+    SSDRTs(datalign(1,3).trials,1)=nccssd;
     SSDRTs(datalign(1,1).trials,2)=sacdelay;
     SSDRTs(datalign(1,3).trials,2)=ncsacdelay;
     SSDRTs(datalign(1,3).trials,3)=0;
     SSDRTs([datalign(1,1).trials datalign(1,2).trials],3)=1;
     [xctr xtach tach rPTc rPTe] = tachCM2(SSDRTs);
     
+    % compare inhibition function and tychometric curve
+    figure(21);
+    probaresp_diff=hist(nccssd,prevssds)'./(hist(nccssd,prevssds)'+hist(ccssd,prevssds)');
+    subplot(2,2,3);
+    plot(prevssds,probaresp_diff,'LineWidth',1.8);   
+    title('Inhibition function','FontName','calibri','FontSize',15);
+    hxlabel=xlabel(gca,'SSD (ms)','FontName','calibri','FontSize',12);
+    set(gca,'Xlim',[0 500],'XTick',[min(xtach):50:max(xtach)],'TickDir','out','box','off'); %'XTickLabel',[50:50:400]
+    hylabel=ylabel(gca,'Fraction cancelled','FontName','calibri','FontSize',12);
+    set(gca,'Ylim',[0 1],'TickDir','out','box','off');
+    
+    subplot(2,2,1);
+    plot(xtach,tach,'LineWidth',1.8);
+    title('Tachometric function','FontName','calibri','FontSize',15);
+    hxlabel=xlabel(gca,'rPT (ms)','FontName','calibri','FontSize',12);
+    set(gca,'Xlim',[min(xtach) max(xtach)],'XTick',[min(xtach):50:max(xtach)],'TickDir','out','box','off'); %'XTickLabel',[50:50:400]
+    hylabel=ylabel(gca,'Fraction cancelled','FontName','calibri','FontSize',12);
+    set(gca,'Ylim',[0 1],'TickDir','out','box','off');
+    
+    % plot rPTs and NSS sac delays
+    % first rPTs
+    subplot(2,2,2);
+    plot(xtach,rPTc,'r')
+    hold on
+    plot(xtach,rPTe,'b')
+    title('Distribution of rPTs','FontName','calibri','FontSize',15);
+    hxlabel=xlabel(gca,'rPT values','FontName','calibri','FontSize',12);
+    set(gca,'Xlim',[min(xtach) max(xtach)],'XTick',[min(xtach):50:max(xtach)],'TickDir','out','box','off'); %'XTickLabel',[50:50:400]
+    hylabel=ylabel(gca,'N','FontName','calibri','FontSize',12);
+    set(gca,'TickDir','out','box','off');
+    legend('rPTc','rPTe');
+    %then prepare and plot saccade latency frequency
+    
+    [saclatquant,saclatxlims]=hist(sacdelay,[0:25:500]);
+    saclatfreq=saclatquant./sum(saclatquant);
+    
+    subplot(2,2,4);
+    plot(saclatxlims,saclatfreq,'Color','k','LineWidth',1.8);
+    title('Saccade Latency','FontName','calibri','FontSize',15);
+    hxlabel=xlabel(gca,'Saccade latency','FontName','calibri','FontSize',12);
+    set(gca,'Xlim',[0 500],'XTick',[0:50:500],'TickDir','out','box','off'); %'XTickLabel',[50:50:400]
+    hylabel=ylabel(gca,'Proportion','FontName','calibri','FontSize',12);
+    curylim=get(gca,'YLim');
+    set(gca,'Ylim',[0 curylim(2)],'TickDir','out','box','off');
+    
+%% matching latency to delays    
     %matchtolat=prevssds+round(mssrt);
     % starting with the most prevalent
     matchlatidx=sacdelay>ssdvalues(ssdtotsidx(end))+round(mssrt);
     
-
-% test no-stop signal (NSS) vs canceled stop-signal (CSS), aligned to saccade/SSRT
+%% test no-stop signal (NSS) vs canceled stop-signal (CSS), aligned to saccade/SSRT
 
     rastersNSS=datalign(1).rasters(matchlatidx,:);
     alignNSS=datalign(1).alignidx;
