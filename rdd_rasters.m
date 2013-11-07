19565abaa1751099eb567277716229203f1a1fd0
Vincent <vincent@server.com>
2013-11-07 00:14:45 -0500

    testing lag with S190 10830

--- rdd_rasters.m
+++ rdd_rasters.m
@@ -555,12 +555,12 @@ while ~islast
                             sktg=sktg+1;
                         end
                         % can re-sync if spikes. Big ones. But otherwise? 
-%                         trrawsig=rawdata.values(find(rawdata.times>=rawtrialtimes(d*2-1+sktg),1):...
-%                             find(rawdata.times>=rawtrialtimes(d*2+sktg),1));
-%                         getspkt= @(x) find(bwlabel(trrawsig>0.7)==x,1)/50;
-%                         if std((find(train,6)-[getspkt(1) getspkt(2) getspkt(3) getspkt(4) getspkt(5) getspkt(6)])) <1
-%                             mean((find(train,6)-[getspkt(1) getspkt(2) getspkt(3) getspkt(4) getspkt(5) getspkt(6)])) %>1? 
-%                         end
+                        trrawsig=rawdata.values(find(rawdata.times>=rawtrialtimes(d*2-1+sktg),1):...
+                            find(rawdata.times>=rawtrialtimes(d*2+sktg),1));
+                        getspkt= @(x) find(bwlabel(trrawsig>0.7)==x,1)/50;
+                        if std((find(train,6)-[getspkt(1) getspkt(2) getspkt(3) getspkt(4) getspkt(5) getspkt(6)])) <1
+                            mean((find(train,6)-[getspkt(1) getspkt(2) getspkt(3) getspkt(4) getspkt(5) getspkt(6)])) %>1? 
+                        end
              
                         alignedrawsigs=cat_variable_size_row(alignedrawsigs,rawdata.values(find(rawdata.times>=rawtrialtimes(d*2-1+sktg),1):...
                             find(rawdata.times>=rawtrialtimes(d*2+sktg),1)));
