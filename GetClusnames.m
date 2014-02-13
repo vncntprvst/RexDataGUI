09951e3370df0334076ef27529ef316b10341699
penguinscontrol <penguinscontrol@gmail.com>
2014-02-12 20:15:16 -0500

    clusters popup 3

--- GetClusnames.m
+++ GetClusnames.m
@@ -2,10 +2,15 @@ function clusnames = GetClusnames( input_args )
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
 global allspk_clus;
-clusnames = cell(1,length(allspk_clus));
+b = []; % count non empty allspk_clus entries
 for a = 1:length(allspk_clus)
-    clusnames{a} = num2str(a);
+    if nansum(nansum(allspk_clus{a}))
+        b = [b a];
+    end
 end
-
+clusnames = cell(1,length(b));
+for a = 1:length(b)
+    clusnames{a} = b(a);
+end
 end
 
