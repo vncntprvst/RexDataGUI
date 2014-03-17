078f63304422bed8eb6ffb98bbdd15d01d1b7b8d
penguinscontrol <penguinscontrol@gmail.com>
2014-03-17 13:16:15 -0400

    comments for cluster names

--- GetClusnames.m
+++ GetClusnames.m
@@ -1,16 +1,33 @@
-function clusnames = GetClusnames( input_args )
+function clusnames = GetClusnames(input_args)
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
-global allspk_clus;
-b = []; % count non empty allspk_clus entries
-for a = 1:length(allspk_clus)
-    if nansum(nansum(allspk_clus{a}))
-        b = [b a];
+global allspk_clus directory slash;
+comments = false;
+try
+    if strcmp(input_args(1),'S')                     
+        fhandle = fopen([directory 'Sixx' slash 'Spike2Exports' slash input_args 'n.txt']);
+    elseif strcmp(input_args(1),'R')
+        fhandle = fopen([directory 'Rigel' slash 'Spike2Exports' slash input_args 'n.txt']);
+    elseif strcmp(input_args(1),'H')
+        fhandle = fopen([directory 'Hilda' slash 'Spike2Exports' slash input_args 'n.txt']);
     end
+    comments = true;
+catch
+    comments = false;
 end
-clusnames = cell(1,length(b));
-for a = 1:length(b)
-    clusnames{a} = b(a);
+if comments
+    
+else
+    b = []; % count non empty allspk_clus entries
+    for a = 1:length(allspk_clus)
+        if nansum(nansum(allspk_clus{a}))
+            b = [b a];
+        end
+    end
+    clusnames = cell(1,length(b));
+    for a = 1:length(b)
+        clusnames{a} = num2str(b(a));
+    end
 end
 end
 
