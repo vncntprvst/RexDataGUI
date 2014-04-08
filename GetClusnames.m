0a12a0a69e000204fc378444e8ef72a07f9cd539
penguinscontrol <penguinscontrol@gmail.com>
2014-04-08 14:51:35 -0400

    Databasing 6

--- GetClusnames.m
+++ GetClusnames.m
@@ -1,9 +1,12 @@
 function clusnames = GetClusnames(input_args)
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
-global allspk_clus directory slash;
-comments = false; % is there an "n" file with names and notes for the clusters?
-    if strcmp(input_args(1),'S')                     
+if strcmp(input_args(end-2:end),'REX')
+    clusnames = {'1',' '};
+else    
+    global allspk_clus directory slash;
+    comments = false; % is there an "n" file with names and notes for the clusters?
+    if strcmp(input_args(1),'S')
         fhandle = fopen([directory 'Sixx' slash 'Spike2Exports' slash input_args(1:end-4) 'n.txt']);
     elseif strcmp(input_args(1),'R')
         fhandle = fopen([directory 'Rigel' slash 'Spike2Exports' slash input_args(1:end-4) 'n.txt']);
@@ -13,25 +16,26 @@ comments = false; % is there an "n" file with names and notes for the clusters?
     if fhandle ~= -1
         comments = true;
     end
-if comments
-    a = 1;
-    clusnames{a} = fgetl(fhandle);
-    while ischar(clusnames{a})
-        a = a+1;
+    if comments
+        a = 1;
         clusnames{a} = fgetl(fhandle);
-    end
-    clusnames{a} = [];
-    fclose(fhandle);
-else
-    b = []; % count non empty allspk_clus entries
-    for a = 1:length(allspk_clus)
-        if nansum(nansum(allspk_clus{a}))
-            b = [b a];
+        while ischar(clusnames{a})
+            a = a+1;
+            clusnames{a} = fgetl(fhandle);
         end
-    end
-    clusnames = cell(1,length(b));
-    for a = 1:length(b)
-        clusnames{a} = num2str(b(a));
+        clusnames{a} = [];
+        fclose(fhandle);
+    else
+        b = []; % count non empty allspk_clus entries
+        for a = 1:length(allspk_clus)
+            if nansum(nansum(allspk_clus{a}))
+                b = [b a];
+            end
+        end
+        clusnames = cell(1,length(b));
+        for a = 1:length(b)
+            clusnames{a} = num2str(b(a));
+        end
     end
 end
 end
